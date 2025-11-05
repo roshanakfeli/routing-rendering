9040b2157f1acf3e27669b0aa2a12c6ad1bec4cb
roshanak <roshanakfeli1@gmail.com>
2025-11-05 23:23:47 +0330

    add news page and its detail

---
+++ app/news/page.tsx
@@ -0,0 +1,22 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
+
+const News = () => {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img src={`/images/news/${newsItem.image}`} alt="" />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default News;
